d878d821a49fb33ba31fbeeb0ee744875aca0565
Victor Mours <victor.mours@gmail.com>
2017-01-16 17:32:26 +0100

    WIP: add test for kata list

---
+++ spec/controllers/katas_controller_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe KatasController do
+
+  describe "#index" do
+    before { create(:kata) }
+
+    it "returns a list of katas" do
+
+
+    end
+  end
+
+end
---
+++ spec/factories/kata.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :kata do
+    github_user_name 'fluency-in'
+    github_repo_name 'ruby-beer-song'
+  end
+end
+
